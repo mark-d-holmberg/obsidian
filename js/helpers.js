b4b82e3d234975bd914b3a1c60fb30c6d0b03310
fyorl <kim.mantas@gmail.com>
2019-10-31 21:01:05 +0000

    Use less verbose handlebar helpers.

--- js/helpers.js
+++ js/helpers.js
@@ -43,55 +43,6 @@ Handlebars.registerHelper('exists', function (arg) {
 	return arg != null;
 });
 
-Handlebars.registerHelper('expr', function (op, ...args) {
-	args.pop();
-
-	if (op === '!') {
-		return !args[0];
-	}
-
-	if (op === '&&') {
-		return args[0] && args[1];
-	}
-
-	if (op === '||') {
-		return args[0] || args[1];
-	}
-
-	if (op === '>') {
-		return args[0] > args[1];
-	}
-
-	if (op === '<') {
-		return args[0] < args[1];
-	}
-
-	if (op === '>=') {
-		return args[0] >= args[1];
-	}
-
-	if (op === '<=') {
-		return args[0] <= args[1];
-	}
-
-	if (op === '===') {
-		return args[0] === args[1];
-	}
-
-	let reducer = null;
-	if (op === '+') {
-		reducer = (acc, x) => acc + x;
-	} else if (op === '*') {
-		reducer = (acc, x) => acc * x;
-	} else if (op === '/') {
-		reducer = (acc, x) => acc / x;
-	}
-
-	if (reducer !== null) {
-		return args.reduce(reducer);
-	}
-});
-
 Handlebars.registerHelper('fancy-checkbox', function (...args) {
 	const options = args.pop();
 	const prop = args.join('.');
