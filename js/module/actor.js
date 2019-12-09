a085e26860e14e21afb37fabd604c5969c1cfbab
Fyorl <kim.mantas@gmail.com>
2019-12-09 17:13:46 +0000

    [#10] Fix for long rest wiping out spell slot uses data.

--- js/module/actor.js
+++ js/module/actor.js
@@ -243,7 +243,7 @@ export class ObsidianActor extends Actor5e {
 
 		for (const level of Object.keys(data.spells)) {
 			if (level.startsWith('spell')) {
-				update[`data.spells.${level}`] = 0;
+				update[`data.spells.${level}.value`] = 0;
 			}
 		}
 
