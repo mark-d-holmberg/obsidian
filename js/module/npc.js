a3e92e4cd999c0485c577c2f5878f3d33dda20a8
fyorl <kim.mantas@gmail.com>
2020-02-25 18:06:14 +0000

    NPCs can now benefit from item effects.

--- js/module/npc.js
+++ js/module/npc.js
@@ -1,3 +1,5 @@
+import {Schema} from './schema.js';
+
 export function prepareNPC (actorData) {
 	if (!actorData.flags) {
 		actorData.flags = {};
@@ -5,9 +7,13 @@ export function prepareNPC (actorData) {
 
 	if (!actorData.flags.obsidian) {
 		actorData.flags.obsidian = {
-			attributes: {init: {ability: 'dex'}},
-			skills: {},
+			attributes: {init: {ability: 'dex'}, ac: {ability1: 'dex', base: 10}, speed: {}},
+			order: {equipment: {root: [], containers: []}},
+			saves: {},
+			skills: {custom: []},
 			sheet: {roll: 'reg'}
 		};
 	}
+
+	actorData.flags.obsidian.version = Schema.VERSION;
 }
