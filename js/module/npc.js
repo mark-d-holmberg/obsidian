411cc5b204ac9af337965261f3461a1ac1eb9da0
fyorl <kim.mantas@gmail.com>
2020-04-10 18:29:15 +0100

    Initial work on NPC sheet: tabs, resizing, description, scroll positions.

--- js/module/npc.js
+++ js/module/npc.js
@@ -8,9 +8,36 @@ export class ObsidianNPC extends ActorSheet5eNPC {
 	static get defaultOptions () {
 		const options = super.defaultOptions;
 		mergeObject(options, {
-			classes: options.classes.concat(['obsidian-window'])
+			classes: options.classes.concat(['obsidian-window']),
+			scrollY: ['.obsidian'],
+			tabs: [{
+				navSelector: 'ul.obsidian-tab-bar',
+				contentSelector: 'form.obsidian',
+				initial: 'stats'
+			}]
 		});
 
 		return options;
+	}
+
+	activateListeners (html) {
+		super.activateListeners(html);
+		console.debug(this.actor);
+	}
+
+	_createEditor (target, editorOptions, initialContent) {
+		editorOptions.content_css =
+			`${CONFIG.TinyMCE.css.join(',')},modules/obsidian/css/obsidian-mce.css`;
+		super._createEditor(target, editorOptions, initialContent);
+	}
+
+	_restoreScrollPositions (html, selectors) {
+		const positions = this._scrollPositions || {};
+		for (const sel of selectors) {
+			const el = this.element.find(sel);
+			if (el.length === 1) {
+				el[0].scrollTop = positions[sel] || 0;
+			}
+		}
 	}
 }
