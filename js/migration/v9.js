f8719e18141e7a27d125f60c9aae66a03fd4078a
fyorl <kim.mantas@gmail.com>
2020-07-03 22:12:15 +0100

    Another fix to tools migration.

--- js/migration/v9.js
+++ js/migration/v9.js
@@ -1,6 +1,6 @@
 export const v9 = {
 	convertTools: function (data) {
-		if (data.flags.obsidian.skills.tools.length) {
+		if (data.flags.obsidian.skills.tools?.length) {
 			data.flags.obsidian.tools.custom = duplicate(data.flags.obsidian.skills.tools);
 		}
 	},
