f04b89231974cdcea0b320c93f4e8e7f182bc458
fyorl <kim.mantas@gmail.com>
2020-12-03 14:00:03 +0000

    [#218] Fix migration for worlds with vehicle actors.

--- js/migration/v11.js
+++ js/migration/v11.js
@@ -2,6 +2,10 @@ import {Rules} from '../rules/rules.js';
 
 export const v11 = {
 	convertSpeed: function (data) {
+		if (data.type === 'vehicle') {
+			return;
+		}
+
 		const oldSpeed = data.flags.obsidian.attributes.speed;
 		const movement = data.data.attributes.movement;
 
