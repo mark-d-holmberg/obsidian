40b6d729e6476593c9b1add03039e20fbb348e2e
fyorl <kim.mantas@gmail.com>
2021-11-07 13:14:06 +0000

    Use RollTerm.FLAVOR_REGEXP to remove flavour from roll formulae that we are trying to convert. Release 5.0.22.

--- js/migration/core.js
+++ js/migration/core.js
@@ -385,8 +385,8 @@ function getScalingEffect (data) {
 function getTerms (formula) {
 	const ops = Object.keys(OPERATORS).concat(['(', ')']);
 	const split = new RegExp(ops.map(term => `\\${term}`).join('|'), 'g');
-	const regex = /\[\w+]/gm;
-	const terms = formula.replace(regex, '').replace(split, term => `;${term};`).split(';');
+	const terms =
+		formula.replace(RollTerm.FLAVOR_REGEXP, '').replace(split, term => `;${term};`).split(';');
 	return terms.map(term => term.trim()).filter(term => term !== '').filter((term, i, arr) => {
 		return !((term === '+') && (arr[i - 1] === '+'));
 	});
